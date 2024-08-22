b3292570fc88417192b21247a05b8614c1fb4cd0
LUCAS GABRIEL NASCIMENTO MEDEIROS <20241148060007@ifrn.local>
2024-08-22 10:21:01 -0300

    começo

---
+++ questões.py
@@ -0,0 +1,5 @@
+# 1. Escreva um programa que leia 10 valores inteiros e armazene em uma lista. O pro-
+#grama deve imprimir no terminal quantos valores pares foram digitados pelo usuário.
+
+# Dica: use o operador “%” para verificar se o número é par (ZERO é neutro, ZERO NÃO
+# É PAR). Exemplos:
